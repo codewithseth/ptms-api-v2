fb4eb0a56b1123a5db8e52e41a1335b9f7de3e27
codewithseth <codewithseth2026@gmail.com>
2026-04-27 16:54:56 +0700

    Update CORS middleware to prioritize production origin over local development

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const PORT = process.env.PORT || 3000;
 // ];
 
 // Middleware
-// app.use(cors({ origin: "https://ptms-hazel.vercel.app", credentials: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ptms-hazel.vercel.app", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
